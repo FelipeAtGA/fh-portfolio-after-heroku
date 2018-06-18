d9827c8ad9c9a7458ad1436f60ff92d1af15bda7
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-17 23:40:39 -0400

    add div and classNames

--- src/components/NavMobil.jsx
+++ src/components/NavMobil.jsx
@@ -17,38 +17,45 @@ const NavMobil = () => {
 
   return (
     <div className="nav_wrapper_mobil">
-      <li><Link to="/">Home</Link></li>
-      <li
-        className="dropdown"
-        onMouseOver={active}
-        onMouseOut={unactive}
-        onFocus={() => undefined}
-        onBlur={() => undefined}
-      >
-        <span>Work &nbsp;<i className="fa fa-caret-down" aria-hidden="true" /></span>
-        <ul
-          className="dropdown-content"
+      <div className="burger">
+        <div className="bar1" />
+        <div className="bar2" />
+        <div className="bar3" />
+      </div>
+      <ul className="display_ul">
+        <li><Link to="/">Home</Link></li>
+        <li
+          className="dropdown"
           onMouseOver={active}
           onMouseOut={unactive}
           onFocus={() => undefined}
           onBlur={() => undefined}
         >
-          <li><Link to="/Webdevpro"> Web Development</Link></li>
-          <li><Link to="/PsAi"> Photoshop / Illustrator</Link></li>
-          <li><Link to="videoAnimation"> Video / Animation</Link></li>
-        </ul>
-      </li>
-      <li><Link to="/about">About</Link></li>
-      <li>
-        <a href="https://github.com/FelipeAtGA" target="_blank" rel="noopener noreferrer">
-          <i className="fa fa-github" aria-hidden="true" />
-        </a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/felipe-hernandez/" target="_blank" rel="noopener noreferrer">
-          <i className="fa fa-linkedin" aria-hidden="true" />
-        </a>
-      </li>
+          <span>Work &nbsp;<i className="fa fa-caret-down" aria-hidden="true" /></span>
+          <ul
+            className="dropdown-content"
+            onMouseOver={active}
+            onMouseOut={unactive}
+            onFocus={() => undefined}
+            onBlur={() => undefined}
+          >
+            <li><Link to="/Webdevpro"> Web Development</Link></li>
+            <li><Link to="/PsAi"> Photoshop / Illustrator</Link></li>
+            <li><Link to="videoAnimation"> Video / Animation</Link></li>
+          </ul>
+        </li>
+        <li><Link to="/about">About</Link></li>
+        <li>
+          <a href="https://github.com/FelipeAtGA" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-github" aria-hidden="true" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/felipe-hernandez/" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-linkedin" aria-hidden="true" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
