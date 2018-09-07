d975eaef0c4218c85a95ab192fa22a84c135f0f8
Felipe Hernandez <felipeneo@hotmail.com>
2018-09-07 17:07:48 -0400

    change component from function to class. Add props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './Navigation';
 import '../css/App.css';
 
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-const Header = () => (
-  <header>
-    <Navigation />
-  </header>
-);
+  render() {
+    return (
+      <header>
+        <Navigation
+          onOrOffNavMobil={this.props.onOrOffNavMobil}
+        />
+      </header>
+    );
+  }
+}
 
 export default Header;
